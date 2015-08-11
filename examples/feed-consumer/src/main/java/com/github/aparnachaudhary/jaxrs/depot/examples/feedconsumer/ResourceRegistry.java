540f78dbb6595cf692dcc704e6b82a293f279fda
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2015-08-11 21:03:35 +0200

    Use CDI startup i.s.o. EJB

--- examples/feed-consumer/src/main/java/com/github/aparnachaudhary/jaxrs/depot/examples/feedconsumer/ResourceRegistry.java
+++ examples/feed-consumer/src/main/java/com/github/aparnachaudhary/jaxrs/depot/examples/feedconsumer/ResourceRegistry.java
@@ -3,35 +3,82 @@ package com.github.aparnachaudhary.jaxrs.depot.examples.feedconsumer;
 import com.github.aparnachaudhary.jaxrs.depot.core.registry.EndpointId;
 import com.github.aparnachaudhary.jaxrs.depot.core.registry.EndpointInfo;
 import com.github.aparnachaudhary.jaxrs.depot.core.registry.EndpointRegistry;
+import com.github.aparnachaudhary.jaxrs.depot.core.registry.event.EndpointAdded;
+import com.github.aparnachaudhary.jaxrs.depot.core.registry.event.EndpointRemoved;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.management.*;
 import java.lang.management.ManagementFactory;
 
 /**
- * Created by Aparna on 8/2/15.
+ * @author Aparna Chaudhary
  */
-@Singleton
-@Startup
+@ApplicationScoped
 public class ResourceRegistry {
 
     private static final Logger LOG = LoggerFactory.getLogger(ResourceRegistry.class);
 
     private static final String NODE_NAME = "jboss.node.name";
     private static final String HTTP_PORT = "jboss.http.port";
+    private static final String PRODUCER_APP_NAME = "feed-producer";
+    private static final String PRODUCER_ENDPOINT_NAME = "producer";
+    private static final String APP_NAME = "feed-consumer";
+    private static final String ENDPOINT_NAME = "consumer";
 
     @Inject
     private EndpointRegistry endpointRegistry;
 
-    @PostConstruct
-    public void register() {
-        EndpointId producerEndpointId = EndpointId.EndpointIdBuilder.newBuilder().setAppName("feed-producer").setEndpointName("producer")
+    /**
+     *
+     * @param init
+     */
+    public void onInitialize(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        LOG.debug("CDI: started : {}", init);
+        register();
+    }
+
+    /**
+     *
+     * @param endpointAdded
+     */
+    public void onEndpointAdded(@Observes EndpointAdded endpointAdded) {
+        EndpointId endpointId = endpointAdded.getEndpointId();
+        LOG.info("=========== endpointAdded: {} ===========", endpointId);
+        if (isProducerEndpoint(endpointId)) {
+            register();
+        }
+    }
+
+    /**
+     *
+     * @param endpointRemoved
+     */
+    public void onEndpointRemoved(@Observes EndpointRemoved endpointRemoved) {
+        EndpointId endpointId = endpointRemoved.getEndpointId();
+        LOG.info("=========== endpointRemoved: {} ===========", endpointId);
+        if (isProducerEndpoint(endpointId)) {
+            if (!dependenciesExist()) {
+                unregister();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param init
+     */
+    public void onDestroy(@Observes @Destroyed(ApplicationScoped.class) Object init) {
+        LOG.info("CDI: destroyed : {}", init);
+    }
+
+    private void register() {
+        EndpointId producerEndpointId = EndpointId.EndpointIdBuilder.newBuilder().setAppName(PRODUCER_APP_NAME).setEndpointName(PRODUCER_ENDPOINT_NAME)
                 .createEndpointId();
         if (endpointRegistry.existsEndpoint(producerEndpointId.getAppName(), producerEndpointId.getEndpointName())) {
             EndpointId consumerEndpointId = getLocalEndpointId();
@@ -44,27 +91,27 @@ public class ResourceRegistry {
         }
     }
 
-    private EndpointId getLocalEndpointId() {
-        return EndpointId.EndpointIdBuilder.newBuilder().setNodeName(System.getProperty(NODE_NAME)).setAppName("feed-consumer")
-                .setEndpointName("consumer")
-                .createEndpointId();
-    }
-
-    @PreDestroy
-    public void unregister() {
+    private void unregister() {
         EndpointInfo consumerEndpointInfo = endpointRegistry.getEndpoint(getLocalEndpointId());
         if (consumerEndpointInfo != null) {
             endpointRegistry.removeEndpoint(getLocalEndpointId());
         }
     }
 
-    public boolean dependenciesExist() {
-        EndpointId producerEndpointId = EndpointId.EndpointIdBuilder.newBuilder().setAppName("feed-producer").setEndpointName("producer")
+    private boolean isProducerEndpoint(EndpointId endpointId) {
+        return endpointId.getAppName().equalsIgnoreCase(PRODUCER_APP_NAME) && endpointId.getEndpointName().equalsIgnoreCase(PRODUCER_ENDPOINT_NAME);
+    }
+
+    private boolean dependenciesExist() {
+        EndpointId producerEndpointId = EndpointId.EndpointIdBuilder.newBuilder().setAppName(PRODUCER_APP_NAME).setEndpointName(PRODUCER_ENDPOINT_NAME)
                 .createEndpointId();
-        if (endpointRegistry.existsEndpoint(producerEndpointId.getAppName(), producerEndpointId.getEndpointName())) {
-            return true;
-        }
-        return false;
+        return endpointRegistry.existsEndpoint(producerEndpointId.getAppName(), producerEndpointId.getEndpointName());
+    }
+
+    private EndpointId getLocalEndpointId() {
+        return EndpointId.EndpointIdBuilder.newBuilder().setNodeName(System.getProperty(NODE_NAME)).setAppName(APP_NAME)
+                .setEndpointName(ENDPOINT_NAME)
+                .createEndpointId();
     }
 
     private String getBaseUri(String contextRoot) {
@@ -73,15 +120,7 @@ public class ResourceRegistry {
             Object port = System.getProperty(HTTP_PORT);
             Object host = ManagementFactory.getPlatformMBeanServer().getAttribute(new ObjectName("jboss.as:interface=public"), "inet-address");
             baseUri = String.format("http:/%s:%s/%s", host, port, contextRoot);
-        } catch (MBeanException e) {
-            e.printStackTrace();
-        } catch (AttributeNotFoundException e) {
-            e.printStackTrace();
-        } catch (InstanceNotFoundException e) {
-            e.printStackTrace();
-        } catch (ReflectionException e) {
-            e.printStackTrace();
-        } catch (MalformedObjectNameException e) {
+        } catch (MBeanException | AttributeNotFoundException | ReflectionException | InstanceNotFoundException | MalformedObjectNameException e) {
             e.printStackTrace();
         }
         return baseUri;
